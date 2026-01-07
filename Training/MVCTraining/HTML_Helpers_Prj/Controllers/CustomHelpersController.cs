a36c0dab75e699cfcfb3d562ba2dccf95f49c35a
Humera Shaikh <humeras@infinite.com>
2026-01-07 09:39:45 +0530

    Custom HTML Helpers

---
+++ Training/MVCTraining/HTML_Helpers_Prj/Controllers/CustomHelpersController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HTML_Helpers_Prj.Controllers
+{
+    public class CustomHelpersController : Controller
+    {
+        // GET: CustomHelpers
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
